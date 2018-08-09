49760b7140d5057d0633a878db87d74d3708e025
sleeepweaker <365768924@qq.com>
2018-08-09 17:08:30 +0800

    页面修改

--- src/pages/frankShow.vue
+++ src/pages/frankShow.vue
@@ -1,6 +1,10 @@
 <template>
+<div>
+<div class="head clear-float">
+        <img @click="to" src="../assets/login-log.png">
+    </div>
 <div class="main">
-    <ol class="clear-float">
+    <!-- <ol class="clear-float">
         <li>
             <h1>
                 Desc:
@@ -11,13 +15,13 @@
             <h1>
                 ID:
             </h1>
-            <span>{{this.dataShow.Desc}}</span>
+            <span>{{this.dataShow.ID}}</span>
         </li>
         <li>
             <h1>
                 Method:
             </h1>
-            <span>{{this.dataShow.Desc}}</span>
+            <span>{{this.dataShow.Method}}</span>
         </li>
         <li>
             <h1>
@@ -27,7 +31,7 @@
                 <span>Name:{{item.Name}}</span>
                 <span>Desc:{{item.Desc}}</span>
             </div>
-        </li>
+        </li> 
         <li>
             <h1>
                 Path:
@@ -53,9 +57,7 @@
             <h1>
                 Resp:
             </h1>
-            <h3>Code:{{this.dataResp.code}}</h3>
-            <h3>Msg:{{this.dataResp.msg}}</h3>
-            <h3>Data:{{this.dataResp.data}}</h3 >
+            <span>{{this.dataShow.Resp}}</span>
         </li>
         <li>
             <h1>
@@ -66,7 +68,107 @@
                 Method:<span>{{item.Method}}</span>
             </div>
         </li>
-    </ol>
+    </ol> -->
+    <div class="main-color">
+    <div class="prjname clear-float">
+        <h1 class="fontcolor">项目名：{{this.dataShow.prj_name}}</h1>
+        <p class="branch">分支:{{this.dataShow.branch}}</p>
+    </div>
+    
+    <div class="desc fontcolor clear-float">
+        <p class="title">接口描述：</p>
+        <p class="message" ref="descTitle" title=''>{{this.dataShow.desc}}</p>
+        <!-- <div class="author clear-float">
+            <div v-for="item in this.dataShow.authors" class="authorMessage">
+                <p>{{item.name}}</p>
+                <p>{{item.mail}}</p>
+            </div>
+        </div> -->
+    </div>
+    <div class="id-resp clear-float">
+    <div class="ID">
+        <div class="IDtitle clear-float">
+            <p class="method">Method</p>
+            <p style="width:70%">Path</p>
+        </div>
+        <div class="IDmessage clear-float">
+            <p class="method">{{this.dataShow.method}}</p>
+            <p style="width:70%">{{this.dataShow.path}}</p>
+        </div>
+    </div>
+    <div class="Resp">
+        <p class="Resptitle">响应:</p>
+        <pre class="Respmessage">{{this.dataShow.resp}}</pre>
+    </div>
+    </div>
+    <div class="header-query clear-float">
+    <div class="Params">
+        <h2>请求头:</h2>
+        <div class="Paramstitle clear-float">
+            <p>key</p>
+            <p>val</p>
+        </div>
+        <div v-for="item in this.dataShow.headers" class="Paramsmessage clear-float">
+            <p>{{item.key}}</p>
+            <p>{{item.val}}</p>
+        </div>
+    </div>
+    <div class="Querys">
+        <h2>URL参数:</h2>
+        <div class="Querystitle clear-float">
+            <p>参数Name</p>
+            <p>参数Desc</p>
+        </div>
+        <div v-for="item in this.dataShow.querys" class="Querysmessage clear-float">
+            <p>{{item.name}}</p>
+            <p>{{item.desc}}</p>
+        </div>
+    </div>
+    </div>
+    
+    <div class="Body">
+        <h2>请求体:</h2>
+        <div class="Bodytitle clear-float">
+            <p>参数Name</p>
+            <p>参数Desc</p>
+        </div>
+        <div v-for="item in this.dataShow.body" class="Bodymessage clear-float">
+            <div class="body-and clear-float" style="height=100%">
+            <p class="p">{{item.name}}</p>
+            <p  class="p">{{item.desc}}</p>
+            </div>
+        </div>
+        <div  class="json-show">
+            <h3>JSON请求体：</h3>
+            <div v-for="item in this.jsonData">
+                <pre>{{item}}</pre>
+            </div>
+        </div>
+    </div>
+    <div class="SubApis">
+        <h2>负责人:</h2>
+        <div class="SubApistitle clear-float">
+            <p class="method">姓名：</p>
+            <p class="click">邮箱:</p>
+        </div>
+        <div v-for="item in this.dataShow.authors" class="SubApismessage clear-float">
+            <p class="method">{{item.name}}</p>
+            <p class="click">{{item.mail}}</p>
+        </div>
+    </div>
+    <div class="SubApis">
+        <h2>依赖接口:</h2>
+        <div class="SubApistitle clear-float">
+            <p style="width:70%">API</p>
+            <p class="method">Method</p>
+        </div>
+        <div v-for="item in this.dataShow.sub_apis" class="SubApismessage clear-float">
+            <p class="click" @click="apiSearch(item.api)">{{item.api}}</p>
+            <p class="method">{{item.method}}</p>
+        </div>
+    </div>
+</div>
+</div>
 </div>
 </template>
 <script>
@@ -77,81 +179,307 @@ export default {
         return {
             includedComponents: "test-keep-alive",
             dataShow:"",
-            dataResp:""
+            dataResp:"",
+            jsonData:[],
         }
     },
     mounted(){
             let _this = this;
             let id = this.$route.query.dataId
-            
             axios.get('http://frank.onenet.com/frank-api?id='+id,{
                 }).then(function(response){
-                _this.dataShow= JSON.parse(response.data.Data);
-                _this.dataResp=JSON.parse(_this.dataShow.Resp)
+                _this.dataShow= JSON.parse(response.data.data);
+                _this.$refs.descTitle.title = _this.dataShow.desc;
+                _this.dataShow.body.forEach(_this.jsonCheck);
+                _this.dataShow.resp = JSON.stringify(JSON.parse(_this.dataShow.resp), null, 4);
                 }).catch(function(err){
                     console.log(err);
                 });
-            
-           
         },
     methods:{
         apiSearch(e){
+           
             this.$router.push({path:"/show",query:{data:e,type:"api"}})
+        },
+        to(){
+            this.$router.push({path:"/show",query:{type:"api"}})
+        },
+        jsonCheck(e){
+            if(e.name == "*json*"){
+                this.jsonData.push(JSON.stringify(JSON.parse(e.desc), null, 4))
+                this.dataShow.body.splice(this.dataShow.body.indexOf(e),1)
+                console.log(this.dataShow.body) 
+            }
         }
     }
 }
 </script>
 <style scoped>
     .clear-float:after{
-    display:block;
-    clear:both;
-    content:"";
-    visibility:hidden;
-    height:0
+        display:block;
+        clear:both;
+        content:"";
+        visibility:hidden;
+        height:0
     }
     .clear-float{
         zoom:1
     }
-    .main{
-        height: 80%;
-        /* width: 80%;
-        margin: 0 auto; */
-        display: -webkit-box;
-        -webkit-box-pack:center;
-        -webkit-box-align:center;
-        -webkit-box-orient: vertical;
-        text-align: center
+    body{
+        background-color: #fdfcf8;
     }
-    h1{
-        width: 600px;
-        text-align: center;
+    .head{
+        width: 100%;
+        padding-left: 50px;
+        padding-top: 5px;
+        margin: 0 auto;
     }
-    li{
+    .head img{
         float: left;
-        width: 600px;
-        height: 150px;
-        list-style: none;
-        border: 1px solid #e1e4e8;
-        margin-top: 20px;
-        margin-right: 30px;
+        width: 150px;
+        cursor: pointer;
     }
-    h3{
-        text-align: left;
-        margin-left: 150px;
-        margin-top: 5px;
+    .main{
+        padding-top: 20px;
+        background-color: #f5f6f7;
+        
     }
-    span{
-        display: inline-block;
-        margin-right: 20px;
+    .main-color{
+        width: 60%;
+        min-width: 1027px;
+        margin: 0 auto;
+        background-color: #fff;
+    }
+    .prjname{
+        width: 90%;
+        margin: 0 auto;
+        padding-bottom: 20px;
+        /* height: 80px; */
+        font-size: 20px;
+    }
+    .branch{
+        float: right;
+        margin-right: 10px;
+        color: #515a6e;
+    }
+    .fontcolor{
+        color: #515a6e;
+    }
+    .desc{
+        /* padding-top: 30px; */
+        width: 90%;
+        margin: 0 auto;
         font-size: 18px;
-        margin-top: 10px;
+        height: 60px;
+        border-bottom: 1px solid #aaa;
+    }
+
+    .desc p{
+        float: left;
+    }
+    .title{
+        width: 12%;
+        font-weight: 900;
+    }
+    .message{
+        /* width: 88%; */
+        font-weight: 900;
+        display: block;
+        text-align: left;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        word-wrap: break-word;
+        word-break: break-all;
+    }
+    .author{
+        float: right;
+        height: 100px;
+    }
+    .authorMessage{
+    }
+    .authorMessage p{
+        margin-left: 10px;
+    }
+    .id-resp{
+        width: 90%;
+        padding-top: 40px;
+        margin: 0 auto;
+        padding-bottom: 40px;
+        border-bottom: 1px solid #aaa;
+    }
+    .ID{
+        float: left;
+        width: 40%;
+    }
+    .IDtitle{
+        background-color: #3F3F3F;
+    }
+    .IDtitle p{
+        float: left;
+        color: #fff;
+        font-weight: bold;
+        padding: 5px 15px 5px 6px;
+        height: 40px;
+        line-height: 40px;
+    }
+    .IDmessage p{
+        float: left;
+        padding: 6px 15px 6px 6px;
+        border: 1px solid #aaa;
+        font-size: 18px
+    }
+    .Resp{
+        float: right;
+        width: 40%;
+    }
+    .Resp p{
+        font-size: 18px;
+    }
+    .Resptitle{
+        text-align: left;
+    }
+    .Respmessage{
+        height: 85px;
+        padding-left: 10px;
+        text-align: left;
+    }
+    .header-query{
+        width: 90%;
+        margin: 0 auto;
+    }
+    .Params{
+        width: 40%;
+        padding-top: 40px;
+        float: left;
+        margin: 0 auto;
+        padding-bottom: 40px;
+        /* border-bottom: 1px solid #aaa; */
+    }
+    .Params h2{
+        text-align: left
+    }
+    .Paramstitle{
+        background-color: #3F3F3F;
+    }
+    .Paramstitle p{
+        float: left;
+        color: #fff;
+        font-weight: bold;
+        padding: 5px 15px 5px 6px;
+        height: 40px;
+        line-height: 40px;
+        width: 50%;
+    }
+    .Paramsmessage p{
+        float: left;
+        width: 50%;
+        padding: 6px 15px 6px 6px;
+        border: 1px solid #aaa;
+        font-size: 18px
+    }
+    .Querys{
+        width: 50%;
+        padding-top: 40px;
+        float: right;
+        margin: 0 auto;
+        padding-bottom: 40px;
+        /* border-bottom: 1px solid #aaa; */
+    }
+    .Querys h2{
+        text-align: left
+    }
+    .Querystitle{
+        background-color: #3F3F3F;
+    }
+    .Querystitle p{
+        float: left;
+        color: #fff;
+        font-weight: bold;
+        padding: 5px 15px 5px 6px;
+        height: 40px;
+        line-height: 40px;
+        width: 50%;
+    }
+    .Querysmessage p{
+        float: left;
+        width: 50%;
+        padding: 6px 15px 6px 6px;
+        border: 1px solid #aaa;
+        font-size: 18px
+    }
+    .SubApis{
+        width: 90%;
+        padding-top: 40px;
+        margin: 0 auto;
+        padding-bottom: 40px;
+    }
+    .SubApis h2{
+        text-align: left
+    }
+    .SubApistitle{
+        background-color: #3F3F3F;
+    }
+    .SubApistitle p{
+        float: left;
+        color: #fff;
+        font-weight: bold;
+        padding: 5px 15px 5px 6px;
+        height: 40px;
+        line-height: 40px;
+        /* width: 50%; */
+    }
+    .SubApismessage p{
+        float: left;
+        /* width: 50%; */
+        padding: 6px 15px 6px 6px;
+        border: 1px solid #aaa;
+        font-size: 18px
     }
     .click{
         cursor: pointer;
-        /* color: aqua; */
+        width: 70%;
     }
-    .divfor{
-        font-size: 18px;
+    .method{
+        width: 30%;
+    }
+        .Body{
+        width: 90%;
+        padding-top: 40px;
+        margin: 0 auto;
+        padding-bottom: 40px;
+    }
+    .Body h2{
+        text-align: left
+    }
+    .Bodytitle{
+        background-color: #3F3F3F;
+    }
+    .Bodytitle p{
+        float: left;
+        color: #fff;
+        font-weight: bold;
+        padding: 5px 15px 5px 6px;
+        height: 40px;
+        line-height: 40px;
+        width: 50%;
+    }
+    .Bodymessage .p{
+        float: left;
+        width: 50%;
+        height: 100%;
+        /* text-align: left; */
+        margin: 0;
+        border-right: 1px solid #aaa;
+        padding: 6px 15px 6px 6px;
+        font-size: 18px
+    }
+    .body-and{
+        border: 1px solid #aaa;
+    }
+    .json-show{
+        text-align: left;
+        border: 1px solid #aaa;
     }
 </style>
 
--- src/pages/show.vue
+++ src/pages/show.vue
@@ -17,8 +17,8 @@
                 Name
                 </Col>
                 <Col span="18">
-                 <Input v-model="valueName" size="large" placeholder="项目名称" style="width: 150px;float:left" @keyup.enter.native="nameSearch('name')"/>
-                <Input v-model="valueAddrs" size="large" placeholder="地址" style="width: 330px;float:left;" @keyup.enter.native="nameSearch('name')"/>
+                 <Input v-model="valueName" size="large" placeholder="项目名称" style="width: 120px;float:left" @keyup.enter.native="nameSearch('name')"/>
+                <Input v-model="valueAddrs" size="large" placeholder="地址" style="width: 300px;float:left;" @keyup.enter.native="nameSearch('name')"/>
                 </Col>
                 <Col span="3">
                 <Button type="primary" icon="ios-search" id="name" @click="nameSearch($event.currentTarget.id)">Search</Button>
@@ -41,62 +41,142 @@
                 nameShow:false,
                 columnsApi: [
                     {
-                        title: 'addr',
-                        key: 'addr',
-                        width:500,
+                        title: 'branch',
+                        key: 'branch',
+                        width:150,
+                    },
+                    {
+                        title: 'method',
+                        key: 'method',
+                        width:180,
                            render: (h, params) => {
-                            return h('a', {
+                            return h('div', {
                                     props: {
                                         type: 'primary',
-                                        size: 'small'
+                                        size: 'big'
+                                    },
+                                    'class':{
+                                        [params.row.method]:true,
                                     },
                                     style:{
                                         display:'block',
-                                        height:'48px',
-                                        lineHeight:'48px'
                                     },
-                                    on: {
-                                        click: () => {
-                                            this.rowClick(params)
-                                        }
-                                    }
-                                },params.row.addr)
-                           }
+                                },params.row.method)
+                           },
+                        filters: [
+                            {
+                                label:'post',
+                                value:'post'
+                            },
+                            {
+                                label:'get',
+                                value:'get'
+                            },
+                            {
+                                label:'put',
+                                value:'put'
+                            },
+                            {
+                                label:'delete',
+                                value:'delete'
+                            },
+                            {
+                                label:'patch',
+                                value:'patch'
+                            },
+                        ],
+                        filterMethod (value, row) {
+                            switch (value) {
+                                case 'post':
+                                    return row.method =='post'
+                                    break;
+                                case 'get':
+                                    return row.method =='get'
+                                    break;
+                                case 'put':
+                                    return row.method =='put'
+                                    break;
+                                case 'delete':
+                                    return row.method =='delete'
+                                    break;
+                                case 'patch':
+                                    return row.method =='patch'
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
                     },
+
                     {
                         title: 'desc',
                         key: 'desc'
                     },
                     {
-                        title: 'id',
-                        key: 'id'
+                        title: 'path',
+                        key: 'path'
+                    },
+                    {
+                        title: 'action',
+                        key: 'action',
+                        width:180,
+                        align: 'center',
+                           render: (h, params) => {
+                            return h('div', {
+                                    props: {
+                                        type: 'primary',
+                                        size: 'big'
+                                    },
+                                    style:{
+                                        display:'block',
+                                        backgroundColor:'#2d8cf0',
+                                        color:'#fff',
+                                        height:'30px',
+                                        lineHeight:'30px',
+                                        borderRadius:'5px',
+                                        cursor:'pointer',
+                                    },
+                                    on: {
+                                        click:()=>{
+                                            this.rowClick(params)
+                                        }
+                                    }
+                                },"预览")
+                           }
                     },
                 ],
                 dataApi: [
+                    // {addr:"xxxxxxxxxxxxxx",id:"123",desc:"231241324",Desc:"adqfdsfeagf",Method:"Post",Params:[{name:"123",path:"rwwrafdew"},{name:"243",path:"1adsfda"}],PrjName:"test"},
+                    // {addr:"xxxxxxxxxxxxxx",id:"123",desc:"231241324",Desc:"adqfdsfeagf",Method:"Get",Params:[{name:"123",path:"rwwrafdew"},{name:"243",path:"1adsfda"}],PrjName:"test"},
+                    // {addr:"xxxxxxxxxxxxxx",id:"123",desc:"231241324",Desc:"adqfdsfeagf",Method:"Put",Params:[{name:"123",path:"rwwrafdew"},{name:"243",path:"1adsfda"}],PrjName:"test"},
+                    // {addr:"xxxxxxxxxxxxxx",id:"123",desc:"231241324",Desc:"adqfdsfeagf",Method:"Delete",Params:[{name:"123",path:"rwwrafdew"},{name:"243",path:"1adsfda"}],PrjName:"test"},
+                    // {addr:"xxxxxxxxxxxxxx",id:"123",desc:"231241324",Desc:"adqfdsfeagf",Method:"Patch",Params:[{name:"123",path:"rwwrafdew"},{name:"243",path:"1adsfda"}],PrjName:"test"},
                 ],
                 dataName: [
                 ],
                 columnsName: [
                     {
-                        title: 'addr',
-                        key: 'addr'
+                        title: 'name',
+                        key: 'name'
                     },
                     {
                         title: 'scheme',
                         key: 'scheme'
                     },
                     {
+                        title: 'addr',
+                        key: 'addr'
+                    },
+                    
+                    {
                         title: 'id',
                         key: 'id'
                     },
-                    {
-                        title: 'name',
-                        key: 'name'
-                    },
+                    
                 ],
             valueApi:"",
             valueName:"",
-            valueAddrs:""
+            valueAddrs:"",
             }
         },
         mounted(){
@@ -110,6 +190,7 @@
                 this.valueName = this.$route.query.dataName
                 this.nameSearch("name")
             }
+            // this.dataApi.map(this.methodColor)
         },
         created:function(){
         let that = this
@@ -142,10 +223,10 @@
             let _this = this
             switch (e) {
                 case "api":
-
                         axios.get('http://frank.onenet.com/search/api?api='+this.valueApi,{
                             }).then(function(response){
-                            _this.dataApi =JSON.parse(response.data.Data)
+                            _this.dataApi =JSON.parse(response.data.data)
+                            console.log(_this.dataApi)
                             })
                             .catch(function(err){
                             console.log(err);
@@ -154,7 +235,7 @@
                 case "name":
                         axios.get('http://frank.onenet.com/search/project?addr='+this.valueAddrs+'&prj_name='+this.valueName,{
                             }).then(function(response){
-                            _this.dataName = JSON.parse(response.data.Data)
+                            _this.dataName = JSON.parse(response.data.data)
                             })
                             .catch(function(err){
                             console.log(err);
@@ -162,12 +243,12 @@
                     break;
                 default:
                     break;
-            }
-        },
+                }
+            },
         }
     }
 </script>
-<style scoped>
+<style >
     .searchIn{
         width:40%;
         margin-top: 20px;
@@ -179,13 +260,66 @@
         font-size: 18px;
         cursor: pointer;
     }
+    /* .searchShow{
+       width: 1050px;
+       margin: 0 auto;
+    } */
     .showTitle Col{
         height: 50px;
         line-height: 50px;
         text-align: center
     }
-    .ivu-table-row{
-        cursor: pointer;
+    .post{
+        color: #fff;
+        background-color: #19be6b;
+        text-align: center;
+        border-radius: 5px;
+        width: 80px;
+        height: 30px;
+        line-height: 30px;
     }
+    .get{
+        color: #fff;
+        background-color: #2d8cf0;
+        text-align: center;
+        border-radius: 5px;
+        width: 80px;
+        height: 30px;
+        line-height: 30px;
+    }
+    .put{
+        color: #fff;
+        background-color: #f90;
+        text-align: center;
+        border-radius: 5px;
+        width: 80px;
+        height: 30px;
+        line-height: 30px;
+    }
+    .delete{
+        color: #fff;
+        background-color: #ed3f14;
+        text-align: center;
+        border-radius: 5px;
+        width: 80px;
+        height: 30px;
+        line-height: 30px;
+    }
+    .patch{
+        color: #fff;
+        background-color: #f90;
+        text-align: center;
+        border-radius: 5px;
+        width: 80px;
+        height: 30px;
+        line-height: 30px;
+    }
+    .ivu-table-row{
+        /* cursor: pointer; */
+    }
+    /* .ivu-table .demo-table-info-cell-method{
+        background-color: #ff6600;
+        color: #fff;
+    } */
 </style>
 
